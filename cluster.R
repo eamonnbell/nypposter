fc15421d970d22fe6c59df2a7dd5b21c93ebafd2
Eamonn Bell <pseudoboarder@gmail.com>
2016-04-04 20:41:58 -0400

    Add a lot.

--- cluster.R
+++ cluster.R
@@ -11,9 +11,11 @@ ctrl <- new("dtwclustControl", window.size = 20L, trace = TRUE)
 
 ctrl@pam.precompute <- FALSE
 
-kc.sbd <- dtwclust(data = d, k = 6, distance="sbd",
-                     centroid = "shape", 
-                     control = ctrl)
+
+## Equivalent to k-Shape
+kc.sbd <- dtwclust(data = d, k = 6, type="partitional", distance="sbd",
+                 centroid = "shape", 
+                 control = ctrl)
 
 kc.sbd
 
@@ -22,3 +24,22 @@ centroids <- attributes(kc.sbd)$centers
 clusters <- attributes(kc.sbd)$cluster
 
 clustered <- data.frame(pieces[250:500], clusters)
+
+
+json.one <- toJSON(centroids[[1]])
+json.two <- toJSON(centroids[[2]])
+json.three <- toJSON(centroids[[3]])
+json.four <- toJSON(centroids[[4]])
+json.five <- toJSON(centroids[[5]])
+json.six <- toJSON(centroids[[6]])
+
+write(json.one, "~/Projects/nypposter/center1.json")
+write(json.two, "~/Projects/nypposter/center2.json")
+write(json.three, "~/Projects/nypposter/center3.json")
+write(json.four, "~/Projects/nypposter/center4.json")
+write(json.five, "~/Projects/nypposter/center5.json")
+write(json.six, "~/Projects/nypposter/center6.json")
+
+write(toJSON(clustered), "~/Projects/nypposter/clusters.json")
+
+
